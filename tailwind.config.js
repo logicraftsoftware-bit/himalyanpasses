d42580d30de78d5c2e96dc8ceb28a1c3df222775
logicraftsoftware-bit <logicraftsoftware@gmail.com>
2026-06-29 18:21:45 +0530

    Replace Tailwind CDN with local CSS build

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./public/**/*.{html,js}",
+    "./*.html",
+    "./controllers/**/*.js",
+    "./includes/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {}
+  }
+};

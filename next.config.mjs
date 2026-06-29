3467eaa45e07b98af50d3329897383631bf74fe8
logicraftsoftware-bit <logicraftsoftware@gmail.com>
2026-06-29 17:44:24 +0530

    Include static site files in Vercel build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   poweredByHeader: false,
+  outputFileTracingIncludes: {
+    "/[[...path]]": [
+      "./*.html",
+      "./config/**/*",
+      "./controllers/**/*",
+      "./favicon/**/*",
+      "./images/**/*",
+      "./includes/**/*",
+      "./style/**/*"
+    ]
+  },
   async headers() {
     return [
       {
